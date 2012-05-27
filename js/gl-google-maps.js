ea81aa1306b4612d905e660943cced78262c98ba
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2012-05-27 09:42:29 -0500

    Added capabilities to save last extent and to reload it on refresh. The last extent is saved to a cookie.

--- js/gl-google-maps.js
+++ js/gl-google-maps.js
@@ -2,11 +2,7 @@ var map;
 
 function initialize() {
 	console.log('Google Maps Init');
-	var myOptions = {
-		center : new google.maps.LatLng(8.9, -80.2),
-		zoom : 8,
-		mapTypeId : google.maps.MapTypeId.ROADMAP
-	};
+	var myOptions = getMapInitOptions();
 	map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
 	google.maps.event.addListener(map, 'center_changed', function() {
 		// 3 seconds after the center of the map has changed, pan back to the
@@ -28,17 +24,37 @@ function initialize() {
 	updateFormDate(map)
 }
 
+function getMapInitOptions() {
+	var myOptions = {
+		center : new google.maps.LatLng(8.9, -80.2),
+		zoom : 8,
+		mapTypeId : google.maps.MapTypeId.ROADMAP
+	};
+	lastExtent = JSON.parse(jQuery.cookie('last-extent'));
+	if (lastExtent != null) {
+		myOptions = {
+			center : new google.maps.LatLng(lastExtent.latitude,
+					lastExtent.longitude),
+			zoom : lastExtent.zoom,
+			mapTypeId : google.maps.MapTypeId.ROADMAP
+		};
+	}
+	return myOptions;
+}
 // $('body').load(initialize);
 function updateFormDate(map) {
 	$("#latitude").attr("value", map.getCenter().lat());
 	$("#longitude").attr("value", map.getCenter().lng());
 	$("#zoom").attr("value", map.getZoom());
-	/*
-	lastExtent ={'latitude' : map.getCenter().lat(),
-			'longitude': map.getCenter().lng(),
-			'zoom' : map.getZoom()};
-	$.cookie('last-extent', lastExtent);
-	*/
+
+	lastExtent = {
+		'latitude' : map.getCenter().lat(),
+		'longitude' : map.getCenter().lng(),
+		'zoom' : map.getZoom()
+	};
+	$.cookie('last-extent', JSON.stringify(lastExtent), { path: '/' });
+	$.cookie('info-test', null);
+
 }
 
 function placeMarker(location) {
